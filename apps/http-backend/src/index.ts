1fd4e8686c05dc47f76c1e85177a5d66c906893d
Siddhant Gawad <siddhantgawad4@gmail.com>
2025-10-05 17:22:24 -0400

    need to join postgres to node

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,69 @@
 import express from "express";
+import { Response,Request } from "express";
+import jwt from "jsonwebtoken";
+import z, { email } from "zod";
+import bcrypt from "bcrypt";
+import env from "dotenv";
 
 const app = express();
+
+const JWT_SECRET= process.env.JWT_SECRET as string;
+
+const validateUserSchema = z.object({
+    email:z.email().min(1).max(50).toLowerCase(),
+    password:z.string().min(8).max(50)
+});
+
+
+
+app.post("/v1/sign-up",(req:Request,res:Response,err)=>{
+    try{
+        const { email, password } = req.body;
+        const data = validateUserSchema.parse({ email, password });
+        if(data){
+
+        }
+    }catch(err){
+        if (err instanceof z.ZodError){
+            err.issues;
+        }
+        
+    }
+})
+
+app.post("/v1/sign-in",async (req:Request,res:Response,err)=>{
+    try{
+        const { email, password } = req.body;
+        const data = validateUserSchema.parse({ email, password });
+        if(data){
+            const userExists = await db.findOne(data.email);
+            if(userExists){
+                const validate = await bcrypt.compare(password,userExists.password);
+                if(validate) {
+                    const userId = userExists._id
+                    await jwt.sign({userId},JWT_SECRET)      
+            }
+
+        }
+    }catch(err){
+        if (err instanceof z.ZodError){
+            err.issues;
+        }
+        
+    }
+})
+
+app.post("/v1/room",(req:Request,res:Response,err)=>{
+    try{
+
+    }catch(err){
+
+    }
+})
+
+
+
+
 
 app.listen(3003);
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,31 @@
-import {WebSocketServer} from "ws";
-
-
+import {WebSocketServer,WebSocket} from "ws";
+import http from "http";
 
 const wss = new WebSocketServer({port: 8080});
 
+
+type RoomId = string;
+
+const socketToRoom = new Map<WebSocket,RoomId>();
+
+const roomToSocket = new Map<RoomId,WebSocket>();
+
+function getRoomSet(roomId:RoomId) : Set<WebSocket>{
+    let set = roomToSocket.get(roomId);
+    if(!set){
+        set = new Set<WebSocket>();
+        roomToSocket.set(roomId,set);
+        console.log(`Created a new room: `${roomId});
+    }
+    return set;
+}
+
+
 wss.on("connection", (ws) => {
+
+
+
+
     ws.on("message", (message) => {
         ws.send(`${message}`);
     })
